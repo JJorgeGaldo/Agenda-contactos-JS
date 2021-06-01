676e1b10261263e96d2f7b40b406327e3ddd11f0
Juan J Galdo Mac <juanjorgegaldo@gmail.com>
2021-06-01 11:38:57 +0200

    Code comments added

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", () =>{
     const contacts = document.querySelector(".contacts")
     const templateContact = document.getElementById("template-contact").content
     const fragment = document.createDocumentFragment() 
-    // First of all I'm going to load the JSON data into the web
-    // For doing so I need to fetch the JSON, then go throw it, and at each iteraction put the info into its place and show it
+    // First of all I'm going to load the JSON data and show it in the web.
+    // For doing so I need to fetch the JSON data.
     const fetchData = async() => {
         try{
             const res = await fetch ("../data.json")
@@ -14,20 +14,32 @@ document.addEventListener("DOMContentLoaded", () =>{
             console.log(err)
         }
     }
-    fetchData()
 
+    // I create this function that go throw the data fetched, and place it into the web
     const putContacts = data =>{
         data.forEach(contact => {
             templateContact.getElementById("template-name").textContent = contact.name
             templateContact.getElementById("template-surname").textContent = contact.surname
             templateContact.getElementById("template-phone").textContent = contact.phone
             templateContact.getElementById("template-mail").textContent = contact.mail
-        
+            
+            /* I create a temporal object that will conteins the info of this iteraction by clonning the template into it */
             const clone = templateContact.cloneNode(true)
+
+            /* I have created an fragment object where Im going to add the clone object of each iteraction, saving this way all the iteractions info before sending it to the web. This way we only send it once */
             fragment.appendChild(clone)
         });
+
+        /* I add the fragment info to the element I want in the HTML (I previously declared the contacts variable). Now the info is shown in the correct place into the HTML */
         contacts.appendChild(fragment)
-        
     }
+
+
+
+    // I call the function to start the process
+    fetchData()
+
+
+
 
 });
